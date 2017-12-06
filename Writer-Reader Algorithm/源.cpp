1beab06e69c75cd82fda4c24c0a41f90acb463f0
4770K <Mr_Ray.Y@live.com>
2017-12-06 20:50:14 +0800

    Complate Reader Thread

--- Writer-Reader Algorithm/源.cpp
+++ Writer-Reader Algorithm/源.cpp
@@ -29,9 +29,53 @@ struct ThreadInfo
 };
 
 
-//读者优先——读者线程
+//读者优先————读者线程
 //P: 读者线程信息
 void RP_ReaderThread(void *p)
+{
+	//互斥变量
+	HANDLE h_Mutex;
+	h_Mutex = OpenMutex(MUTEX_ALL_ACCESS, FALSE, "mutex_for_readcount");
+	DWORD wait_for_mutex;	//等待互斥变量所有权
+	DWORD m_delay;	//延迟时间
+	DWORD m_persist;	//读文件持续时间
+	int m_serial;	//线程序列号
+	//从参数中获得信息
+	m_serial = ((ThreadInfo*)(p))->serial;
+	m_delay = (DWORD)(((ThreadInfo*)(p))->delay *INTE_PER_SEC);
+	m_persist = (DWORD)(((ThreadInfo*)(p))->persist *INTE_PER_SEC);
+	Sleep(m_delay);	//延迟等待
+	printf("读线程 %d 发出读请求。\n", m_serial);
+	//等待互斥信号，保证对 ReadCount 的访问，修改互斥
+	wait_for_mutex = WaitForSingleObject(h_Mutex, -1);
+	//读者数量增加
+	readcount++;
+	if (readcount==1)
+	{
+		//第一个读者，等待资源
+		EnterCriticalSection(&RP_Write);
+	}
+	ReleaseMutex(h_Mutex);	//释放互斥信号
+	//读文件
+	printf("读线程 %d 开始读文件。\n", m_serial);
+	Sleep(m_persist);
+	//退出线程
+	printf("读线程 %d 结束读文件。\n", m_serial);
+	//等待互斥信号，保证对 ReadCount 的访问，修改互斥
+	wait_for_mutex = WaitForSingleObject(h_Mutex, -1);
+	//读者数量减少
+	readcount--;
+	if (readcount==0)
+	{
+		LeaveCriticalSection(&RP_Write);	//如果所有读者读完，唤醒写者
+	}
+	ReleaseMutex(h_Mutex);	//释放信号量
+}
+
+
+//读者优先————写者进程
+//P: 写者信息
+void RP_WriterThread(void *p)
 {
 
 }
